27812a83663f47407a3f24166f3f7d0e21a0daf3
GorosVia <gorosvia@gmail.com>
2015-06-09 16:27:17 +0400

    Improve solution and bug fix

--- prac4ce/src/ifmo/wordlab/Question.java
+++ prac4ce/src/ifmo/wordlab/Question.java
@@ -3,19 +3,29 @@ package ifmo.wordlab;
 public class Question{
 	public String questDesc = null;
 	public Integer questNumAns = null;
+	public Double questFloatAns = null;
 	public String questStrAns = null;
 	boolean checkAnswer(String ans) throws NumberFormatException, QuestWrongAnsException{
+		if (ans == null) return false;
+		
 		if (questNumAns != null){
-				int ansNum = Integer.parseInt(ans);
-				if (ansNum != questNumAns)
+			int ansNum = Integer.parseInt(ans);
+			if (ansNum != questNumAns)
+				throw new QuestWrongAnsException();
+		}
+		if (questFloatAns != null){
+				double ansNum = Double.parseDouble(ans);
+				if (ansNum != questFloatAns)
 					throw new QuestWrongAnsException();
 		}
 		if (questStrAns != null){
 			if (!ans.equals(questStrAns))
 				throw new QuestWrongAnsException();
 		}
+		
 		return true;
 	}
+	
 	Question(String description, String answer){
 		questDesc = new String(description);
 		questStrAns = new String(answer);
@@ -23,5 +33,9 @@ public class Question{
 	Question(String description, Integer answer){
 		questDesc = new String(description);
 		questNumAns = new Integer(answer);
+	}
+	Question(String description, Double answer){
+		questDesc = new String(description);
+		questFloatAns = new Double(answer);
 	}
 }
--- prac4ce/src/ifmo/wordlab/WordLabServlet.java
+++ prac4ce/src/ifmo/wordlab/WordLabServlet.java
@@ -8,24 +8,6 @@ import javax.servlet.http.*;
 @SuppressWarnings("serial")
 public class WordLabServlet extends HttpServlet {
 
-	private static int fillQuestionsList(ArrayList<Question> qList){
-		Question currQestion = null;
-		int num = 0;
-		if (qList != null)
-		{
-			currQestion = new Question("Какой же цвет в космосе?","чёрный");
-			qList.add(currQestion);
-			num++;
-
-			currQestion = new Question("В каком-же году родился Пушкин?",1799);
-			qList.add(currQestion);
-			num++;
-
-		} else
-			System.out.println("Null QuesionsList pointer");
-		return num;
-	}
-	
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException{
 	
 		resp.setContentType("text/plain");
@@ -44,9 +26,8 @@ public class WordLabServlet extends HttpServlet {
 			resp.getWriter().println("<p>"
 					+ questList.get(currQnum).questDesc
 					+ "</p>\n"
-					+ "<form>\n"
+					+ "<form align = \"center\">\n"
 					+ "<input type=\"text\" class=\"NumericInput\" id=\"WordAnswer\" placeholder=\"Ответ\">\n"
-					+ "<input type=\"button\" id=\"WordAnswerBtn\" value=\"Показать\">\n"
 					+ "</form>");
 		} else {
 			// Check answer
@@ -78,5 +59,62 @@ public class WordLabServlet extends HttpServlet {
 					resp.getWriter().println("Ответ неверен");
 				}
 		}
+	}
+	
+	
+	private static int fillQuestionsList(ArrayList<Question> qList){
+		Question currQestion = null;
+		int num = 0;
+		if (qList != null)
+		{
+			currQestion = new Question("Какой же цвет неба в космосе?","чёрный");
+			qList.add(currQestion);
+			num++;
+			
+			// (c) Гарднер
+			currQestion = new Question("Пассажирский лайнер \"Клара Люксембург\" отошел от берега на 10 км и "
+					+ "утонул. Капитан поплыл к берегу со скоростью 6 км/ч. Матросы поплыли к "
+					+ "берегу со скоростью 2.4 км/ч, а пассажиры — со скоростью 1.5 км/ч. На сколько "
+					+ "раньше, чем матросы, доберется до берега капитан, и на сколько раньше, чем "
+					+ "пассажиры, доберутся до берега матросы? ", 2.5);
+			qList.add(currQestion);
+			num++;
+			
+			currQestion = new Question("За два часа работы бульдозер разогнал 256 участников "
+					+ "несанкционированного митинга. Сколько участников митинга разгонит бульдозер "
+					+ "за пять часов, если будет работать с прежней производительностью?", 384);
+			qList.add(currQestion);
+			num++;
+			
+			currQestion = new Question("Семья, состоящая из дедушки, бабушки, мамы, папы и четверых детей, "
+					+ "получила по талонам кусок колбасы длиной в 20 сантиметров. Детям отрезали по "
+					+ "4 см каждому. Остальное поровну разделили между взрослыми. По скольку "
+					+ "сантиметров колбасы получил каждый взрослый?", 1);
+			qList.add(currQestion);
+			num++;
+
+			currQestion = new Question("Генерал-майор с генерал-полковником распили бутылку коньяка пятилетней выдержки. "
+					+ "Сколько звездочек можно было насчитать во время этого события?", 13);
+			qList.add(currQestion);
+			num++;
+			
+			currQestion = new Question("Во время охоты в заповеднике 9 секретарей обкомов партии убили "
+					+ "некоторое количество лосей. Сколько лосей было убито, если известно, что "
+					+ "после того, как 5 лучших лосиных рогов было послано в подарок Генеральному "
+					+ "секретарю, каждый секретарь обкома получил по рогам?", 14);
+			qList.add(currQestion);
+			num++;
+
+			currQestion = new Question("Злая колдунья превратилась в Белоснежку и испекла для 7 гномов "
+					+ "40 пирожков с гвоздями. Три гнома отказались от угощения, а остальные "
+					+ "разделили пирожки поровну и кинули их в колдунью. Половина пирожков, "
+					+ "брошенных каждым гномом, попала в колдунью, а другая половина пролетела мимо"
+					+ "нее. Сколько пирожков с гвоздями попало в колдунью?", 20);
+			qList.add(currQestion);
+			num++;
+
+		} else
+			System.out.println("Null QuesionsList pointer");
+		return num;
 	}
 }
