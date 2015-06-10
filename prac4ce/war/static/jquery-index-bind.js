5709d559ac21ee675b10a05594f73578922f0708
GorosVia <gorosvia@gmail.com>
2015-06-11 02:30:01 +0400

    Final sitepage update

--- prac4ce/war/static/jquery-index-bind.js
+++ prac4ce/war/static/jquery-index-bind.js
@@ -1,5 +1,3 @@
-/*Todo: add urlencode*/
-
 $(document).ready(function() {
 
 	/* Practice #7 - to 09.06.15 */
@@ -8,8 +6,8 @@ $(document).ready(function() {
 		$('#OpenData .block + .response').html("");
 	});
 	$('#OpenDataGetLog').on('click',function(){
-		$('#OpenData .block + .response').load( "/opendata?"+
-		                                        "answer="+$("#WordAnswer").val());
+		$('#OpenData .block + .response').load( encodeURI("/opendata?"+
+		                                        "answer="+$("#WordAnswer").val()));
 	});
 
 	/* Practice #6 - to xx.05.15 */
@@ -21,8 +19,8 @@ $(document).ready(function() {
 		$('#WordLab .block + .response').load( "/wordlab");
 	});
 	$('#WordAnswerBtn').on('click',function(){
-		$('#WordLab .block + .response').load( "/wordlab?"+
-		                                        "answer="+$("#WordAnswer").val());
+		$('#WordLab .block + .response').load( encodeURI("/wordlab?"+
+		                                        "answer="+$("#WordAnswer").val()));
 	});
 
 
@@ -41,23 +39,23 @@ $(document).ready(function() {
 		$('#StruLab .block + .response').load( "/structlab?action=3");
 	});
 	$('#StruLabCreDep').on('click',function(){
-		$('#StruLab .block + .response').load( "/structlab?action=0"+
-		                         "&dname="+$("#StruLabDepName").val());
+		$('#StruLab .block + .response').load( encodeURI("/structlab?action=0"+
+		                         "&dname="+$("#StruLabDepName").val()));
 	});
 
 
 	/* Practice #4 - to 28.04.15 */
 
 	$('#UniTableBtn').on('click',function(){
-		$('#UniTable .block + .response').load( "/unitable?"+
+		$('#UniTable .block + .response').load( encodeURI("/unitable?"+
 		                      "min="+$("#UniMin").val()+
-		                     "&max="+$("#UniMax").val());
+		                     "&max="+$("#UniMax").val()));
 	});
 	$('#UniTableBtnAdData').on('click',function(){
-		$('#UniTable .block + .response').load( "/unitable?"+
+		$('#UniTable .block + .response').load( encodeURI("/unitable?"+
 		                      "min="+$("#UniMin").val()+
 		                     "&max="+$("#UniMax").val()+
-		                     "&extmode=true");
+		                     "&extmode=true"));
 	});
 	$('#UniTableBtnHide').on('click',function(){
 		$('#UniTable .block + .response').html("");
@@ -77,8 +75,8 @@ $(document).ready(function() {
 	$('#BuBtnCalc').on('click',function(){
 		var Request = "Count="+MaxCellID;
 		for (Count = 0; Count <= MaxCellID; Count++){
-			Request += "&Desc"+Count+"="+$('#BuDesc'+Count).val();
-			Request += "&Sum" +Count+"="+$('#BudSum'+Count).val();
+			Request += "&Desc"+Count+"="+encodeURI($('#BuDesc'+Count).val());
+			Request += "&Sum" +Count+"="+encodeURI($('#BudSum'+Count).val());
 		}
 		$('#ListBudget .block + .response').load("/listbudget?"+Request);
 	});
@@ -100,9 +98,9 @@ $(document).ready(function() {
 	/*  Colors - lab from lecture at 28.04.15 */
 
 	$('#ColorTableBtn').on('click',function(){
-		$('#ColorTable .block + .response').load( "/colortable?"+
+		$('#ColorTable .block + .response').load( encodeURI("/colortable?"+
 		                      "min="+$("#ColorMin").val()+
-		                     "&max="+$("#ColorMax").val());
+		                     "&max="+$("#ColorMax").val()));
 	});
 	$('#ColorTableBtnHide').on('click',function(){
 		$('#ColorTable .block + .response').html("");
@@ -112,9 +110,9 @@ $(document).ready(function() {
 	/* Practice #3 - to 07.04.15 */
 
 	$('#NearTenBtn').on('click',function(){
-		$('#NearTen .block + .response').load( "/nearten?"+
+		$('#NearTen .block + .response').load( encodeURI("/nearten?"+
 		                         "M="+$("#NearTenM").val()+
-		                        "&N="+$("#NearTenN").val());
+		                        "&N="+$("#NearTenN").val()));
 	});
 	$('#NearTenBtnHide').on('click',function(){
 		$('#NearTen .block + .response').html("");
@@ -123,10 +121,10 @@ $(document).ready(function() {
 
 
 	$('#SquareRootsBtn').on('click',function(){
-		$('#SquareRoots .block + .response').load( "/squaroots?"+
+		$('#SquareRoots .block + .response').load( encodeURI("/squaroots?"+
 		                         "A="+$("#SquareRootsA").val()+
 		                        "&B="+$("#SquareRootsB").val()+
-		                        "&C="+$("#SquareRootsC").val());
+		                        "&C="+$("#SquareRootsC").val()));
 	});
 	$('#SquareRootsBtnHide').on('click',function(){
 		$('#SquareRoots .block + .response').html("");
@@ -145,6 +143,15 @@ $(document).ready(function() {
 	});
 
 
+	$(".tile").hover(function() {
+			$(this).stop().animate({backgroundColor: "#E1ADC9", borderColor: "#BF6797" }, 200);
+			$(".num",this).stop().animate({color: "#B76391"}, 200);
+			$("a,.desc",this).stop().animate({color: "#55002E"}, 200);
+		},function() {
+			$(this).stop().animate({ backgroundColor: "#E6FBBD", borderColor: "#B9DC77" }, 200);
+			$(".num",this).stop().animate({color: "#B3D374"}, 200);
+			$("a,.desc",this).stop().animate({color: "#406200"}, 200);
+	});
 
 	var linkCallback = 0;
 	$('a[href^="#"]').click( function(event){
@@ -162,6 +169,8 @@ $(document).ready(function() {
 		var url = window.location.hash;
 		$("div.task").hide();
 		if (url != ""){
+			if (url == "#dash")
+				url = "";
 			$("div.task" + url).fadeIn();
 			if (linkCallback == 1){
 				$('html, body').stop().animate({ scrollLeft: 0,
@@ -170,6 +179,8 @@ $(document).ready(function() {
 				linkCallback = 0;
 			} else
 				window.scrollTo(0,$("div.task" + url).offset().top);
+			if (url == "")
+				$("div.task#index").hide();
 		} else
 			$("div.task#index").fadeIn();
 	}).trigger('hashchange');
