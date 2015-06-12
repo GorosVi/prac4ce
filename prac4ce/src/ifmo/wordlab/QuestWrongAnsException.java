675410195778bff15a6c713fdf7d2aeb75919edc
GorosVia <gorosvia@gmail.com>
2015-06-12 21:29:14 +0400

    Fix fontset and minor bugfix

--- prac4ce/src/ifmo/wordlab/QuestWrongAnsException.java
+++ prac4ce/src/ifmo/wordlab/QuestWrongAnsException.java
@@ -1,6 +1,8 @@
 package ifmo.wordlab;
 
 public class QuestWrongAnsException extends Exception{
+	private static final long serialVersionUID = 1L;
+
 	public QuestWrongAnsException(){
 		super("Неверный ответ");
 	}
